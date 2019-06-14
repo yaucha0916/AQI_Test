41fb6d5ef152dc2b2c6e41a670e6f9611ec049b8
NickYeh <yaucha0916@gmail.com>
2019-06-14 13:01:04 +0800

    Initial Commit

---
+++ AQI_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AQI_Test
+//
+//  Created by 葉育彰 on 2019/6/14.
+//  Copyright © 2019 葉育彰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
